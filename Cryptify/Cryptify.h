e6a2f5abcc621538c003aa7bb8a963595918d854
Simon Krüger <simon.krueger@intive.com>
2018-10-10 09:22:16 +0200

    Initial Commit

---
+++ Cryptify/Cryptify.h
@@ -0,0 +1,19 @@
+//
+//  Cryptify.h
+//  Cryptify
+//
+//  Created by Simon Krüger on 10.10.18.
+//  Copyright © 2018 cr0ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cryptify.
+FOUNDATION_EXPORT double CryptifyVersionNumber;
+
+//! Project version string for Cryptify.
+FOUNDATION_EXPORT const unsigned char CryptifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cryptify/PublicHeader.h>
+
+
